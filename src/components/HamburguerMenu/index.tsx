baff371ffe4151faa675f62059355e4adce86529
Jefferson Gerson dos Santos <jefferson.santos@maxmilhas.com.br>
2023-08-19 12:54:47 -0300

    refactor: improvements in the button, card, input and hamburger menu components

--- src/components/HamburguerMenu/index.tsx
+++ src/components/HamburguerMenu/index.tsx
@@ -3,66 +3,77 @@ import styles from "./styles.module.scss";
 import { IoCloseOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
-const HamburguerMenu = ({ menuBackground }: { menuBackground?: string }) => {
+interface MenuItem {
+  name: string;
+  icon: string;
+  path: string;
+}
+
+interface HamburguerMenuProps {
+  menuBackground?: string;
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: "Início",
+    icon: "fa-home",
+    path: "/",
+  },
+  {
+    name: "Água",
+    icon: "fa-tint",
+    path: "/consumo-agua",
+  },
+  {
+    name: "Energia",
+    icon: "fa-bolt",
+    path: "/consumo-energia",
+  },
+  {
+    name: "Relatórios",
+    icon: "fa-pie-chart",
+    path: "/relatorios",
+  },
+  {
+    name: "Configurações",
+    icon: "fa-gear",
+    path: "/configuracoes",
+  },
+  // {
+  //   name: "Conta",
+  //   icon: "fa-user",
+  //   path: "/login",
+  // },
+];
+
+const HamburguerMenu: React.FC<HamburguerMenuProps> = ({
+  menuBackground = "#096285da",
+}) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [userName, setUserName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
+  const [userData, setUserData] = useState<{
+    name: string;
+    email: string;
+  } | null>(null);
+  const currentYear = new Date().getFullYear();
 
   const getUserData = () => {
-    const name = localStorage.getItem('userName') || "";
-    const email = localStorage.getItem('userEmail') || "";
+    const name = localStorage.getItem("userName") || "";
+    const email = localStorage.getItem("userEmail") || "";
 
     if (name && email) {
-      setUserName(name);
-      setUserEmail(email);
+      setUserData({ name, email });
     }
-  }
+  };
 
   useEffect(() => {
     getUserData();
-  })
-
-  
-  const menuItems = [
-    {
-      name: "Início",
-      icon: "fa-home",
-      path: "/",
-    },
-    {
-      name: "Água",
-      icon: "fa-tint",
-      path: "/consumo-agua",
-    },
-    {
-      name: "Energia",
-      icon: "fa-bolt",
-      path: "/consumo-energia",
-    },
-    {
-      name: "Relatórios",
-      icon: "fa-pie-chart",
-      path: "/relatorios",
-    },
-    {
-      name: "Configurações",
-      icon: "fa-gear",
-      path: "/configuracoes",
-    },
-    // {
-    //   name: "Conta",
-    //   icon: "fa-user",
-    //   path: "/login",
-    // },
-  ];
+  }, []);
 
   return (
     <>
       <div
         className={`${styles.menuBtn} ${isOpen ? "open" : ""}`}
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
+        onClick={() => setIsOpen(!isOpen)}
       >
         <div className={styles.menuBtnBurguer}></div>
       </div>
@@ -71,14 +82,12 @@ const HamburguerMenu = ({ menuBackground }: { menuBackground?: string }) => {
         className={`${styles.content}`}
         style={{
           display: isOpen ? "flex" : "none",
-          background: menuBackground ? menuBackground : "#096285da",
+          background: menuBackground,
         }}
       >
         <div>
           <div
-            onClick={() => {
-              setIsOpen(false);
-            }}
+            onClick={() => setIsOpen(false)}
             tabIndex={0}
             className={styles.closeBtn}
           >
@@ -87,19 +96,18 @@ const HamburguerMenu = ({ menuBackground }: { menuBackground?: string }) => {
           <div className={styles.userInfo}>
             <h3>Meu Consumo</h3>
             <div className={styles.userInfoContent}>
-              {userName && userEmail ?
-              <div>
-                  <p>{userName}</p>
-                  <span>{userEmail}</span>
-              </div>
-              : 
-              <div className={styles.loginInfo}>
-                <Link to={"/login"}>Entrar</Link>
-                <span> ou </span>
-                <Link to={"/cadastrar"} >Criar conta</Link>
-              </div>
-              }
-              
+              {userData ? (
+                <div>
+                  <p>{userData.name}</p>
+                  <span>{userData.email}</span>
+                </div>
+              ) : (
+                <div className={styles.loginInfo}>
+                  <Link to={"/login"}>Entrar</Link>
+                  <span> ou </span>
+                  <Link to={"/cadastrar"}>Criar conta</Link>
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -121,7 +129,7 @@ const HamburguerMenu = ({ menuBackground }: { menuBackground?: string }) => {
         </ul>
 
         <footer className={styles.footer}>
-          <p>&copy; Meu Consumo - 2022</p>
+          <p>&copy; Meu Consumo - {currentYear}</p>
           <p>Todos os direitos reservados.</p>
         </footer>
       </section>
